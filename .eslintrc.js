fc10c7331d089c6ca02804da60d920be9f85c00d
Birte05 <63093822+Birte05@users.noreply.github.com>
2021-01-10 18:30:50 +0100

    added documentation incl jsdoc

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    'react-native/react-native': true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-native',
+  ],
+  rules: {
+  },
+};
